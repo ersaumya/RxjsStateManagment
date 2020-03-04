800368313027f1190b4c65ae13ba96eba0610393
ersaumya <ersaumyarout@gmail.com>
2020-03-05 01:09:09 +0530

    observable store added

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Store } from './store';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  todos$=this.store.select<any[]>('todos');
+
+  constructor(private store:Store){
+    this.store.set('todos',[{id:1,name:'Eat dinner'},{id:2,name:'washing cloth'}]);
+  }
+
+}
